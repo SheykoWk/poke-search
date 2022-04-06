3647ab687aacd7d27ad1144440ec6cb3cd441705
SheykoWk <sheyko.wk@gmail.com>
2022-04-05 21:02:43 -0500

    radio imput

--- src/App.js
+++ src/App.js
@@ -1,33 +1,57 @@
-import { useState } from 'react';
-import './App.css';
-import Menu from './components/Menu';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+    const [currentAnswer, setCurrentAnswer] = useState(null);
 
-  const [value, setValue] = useState(false) 
-  const [value2, setValue2] = useState(false)
+    console.log(currentAnswer);
 
-  return (
-    <div className="App">
-      <header className="App-header">
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Cual es un ejemplo de renderizado condicional</h1>
+                <input
+                    type="radio"
+                    name="answer"
+                    id="a"
+                    checked={"a" === currentAnswer}
+                    onChange={() => setCurrentAnswer("a")}
+                />
+                <label htmlFor="a">{"value ? <Home /> : null"}</label>
 
-        <input type='checkbox' id='menuToggle' value={value} onChange={(e) => {
-          setValue(e.target.checked)}} /> 
-        <label htmlFor='menuToggle'>
-          Menu Toggle
-        </label>
+                <input
+                    type="radio"
+                    name="answer"
+                    id="b"
+                    checked={"b" === currentAnswer}
+                    onChange={() => setCurrentAnswer("b")}
+                />
+                <label htmlFor="b">
+                    {"isLogged && isAdmin <Admin /> : <Login />"}
+                </label>
 
+                <input
+                    type="radio"
+                    name="answer"
+                    id="c"
+                    checked={"c" === currentAnswer}
+                    onChange={() => setCurrentAnswer("c")}
+                />
+                <label htmlFor="c">
+                    {"value || value2 ?  <Home /> : null"}
+                </label>
 
-        <input type='checkbox' id='menuToggle2' value={value2} onChange={(e) => {
-          setValue2(e.target.checked)}} /> 
-        <label htmlFor='menuToggle2'>
-          Menu Toggle
-        </label>
-
-        { value && value2 ? <Menu /> : null }
-      </header>
-    </div>
-  );
+                <input
+                    type="radio"
+                    name="answer"
+                    id="d"
+                    checked={"d" === currentAnswer}
+                    onChange={() => setCurrentAnswer("d")}
+                />
+                <label htmlFor="d">Todas las anteriores</label>
+            </header>
+        </div>
+    );
 }
 
 export default App;
