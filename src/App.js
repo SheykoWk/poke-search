53dbc96cf728a07052f97a3ee5770ba89823ae10
SheykoWk <sheyko.wk@gmail.com>
2022-04-05 21:32:40 -0500

    start with the pokesearch

--- src/App.js
+++ src/App.js
@@ -1,60 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import getAllPokemons from "./services/getAllPokemons";
+import Pokemon from './components/Pokemon';
 
 function App() {
-    const [currentAnswer, setCurrentAnswer] = useState(null);
+    const [pokemons, setPokemons] = useState([]);
+
+    useEffect(() => {
+        getAllPokemons()
+          .then((response) => {
+            setPokemons(response.data.results)
+          })
+          .catch((err) => {
+            console.error(err)
+          })
+    }, [])
+
+    const list = pokemons.map((pokemon) => <Pokemon name={pokemon.name} />)
 
     return (
         <div className="App">
             <header className="App-header">
-                <h1>Cual es un ejemplo de renderizado condicional</h1>
-                <input
-                    type="radio"
-                    name="answer"
-                    id="a"
-                    checked={"a" === currentAnswer}
-                    onChange={() => setCurrentAnswer("a")}
-                />
-                <label htmlFor="a">{"value ? <Home /> : null"}</label>
-
-                <input
-                    type="radio"
-                    name="answer"
-                    id="b"
-                    checked={"b" === currentAnswer}
-                    onChange={() => setCurrentAnswer("b")}
-                />
-                <label htmlFor="b">
-                    {"isLogged && isAdmin <Admin /> : <Login />"}
-                </label>
-
-                <input
-                    type="radio"
-                    name="answer"
-                    id="c"
-                    checked={"c" === currentAnswer}
-                    onChange={() => setCurrentAnswer("c")}
-                />
-                <label htmlFor="c">
-                    {"value || value2 ?  <Home /> : null"}
-                </label>
-
-                <input
-                    type="radio"
-                    name="answer"
-                    id="d"
-                    checked={"d" === currentAnswer}
-                    onChange={() => setCurrentAnswer("d")}
-                />
-                <label htmlFor="d">Todas las anteriores</label>
-
-                <button onClick={() => {
-                  if(currentAnswer === 'd'){
-                    alert('Correcto felicidades, te has graduado de React')
-                  } else {
-                    alert('Nimodo mi chavo, nos vemos el siguiente modulo')
-                  }
-                }}>Validar Respuesta</button>
 
             </header>
         </div>
