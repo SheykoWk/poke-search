0d6da1a7de16934576674e40299a97bb663727e5
SheykoWk <sheyko.wk@gmail.com>
2022-04-05 19:52:51 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Menu from './components/Menu';
 
 function App() {
+
+  const [value, setValue] = useState(false) 
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type='checkbox' id='menuToggle' value={value} onChange={(e) => setValue(e.target.checked)} /> 
+        <label htmlFor='menuToggle'>Desplegar menu</label>
+        { value ? <Menu /> : null }
       </header>
     </div>
   );
---
+++ src/components/Menu.js
@@ -0,0 +1,11 @@
+const Menu = () => {
+    return(
+        <ul>
+            <li>Contacto</li>
+            <li>Portafolio</li>
+            <li>Home</li>
+        </ul>
+    )
+}
+
+export default Menu
