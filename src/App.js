401202bfbe6bfbcfb8efce3cca20f615c0bc2b86
SheykoWk <sheyko.wk@gmail.com>
2022-04-05 20:14:58 -0500

    renderizado condicional con operadores and y or

--- src/App.js
+++ src/App.js
@@ -5,15 +5,26 @@ import Menu from './components/Menu';
 function App() {
 
   const [value, setValue] = useState(false) 
-
-
+  const [value2, setValue2] = useState(false)
 
   return (
     <div className="App">
       <header className="App-header">
-        <input type='checkbox' id='menuToggle' value={value} onChange={(e) => setValue(e.target.checked)} /> 
-        <label htmlFor='menuToggle'>Desplegar menu</label>
-        { value ? <Menu /> : null }
+
+        <input type='checkbox' id='menuToggle' value={value} onChange={(e) => {
+          setValue(e.target.checked)}} /> 
+        <label htmlFor='menuToggle'>
+          Menu Toggle
+        </label>
+
+
+        <input type='checkbox' id='menuToggle2' value={value2} onChange={(e) => {
+          setValue2(e.target.checked)}} /> 
+        <label htmlFor='menuToggle2'>
+          Menu Toggle
+        </label>
+
+        { value && value2 ? <Menu /> : null }
       </header>
     </div>
   );
