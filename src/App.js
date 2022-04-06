e0033acc2f4370057e1402c1f1cd3300aec5104c
SheykoWk <sheyko.wk@gmail.com>
2022-04-05 21:54:14 -0500

    start with the pokesearch 2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import getAllPokemons from "./services/getAllPokemons";
 import Pokemon from './components/Pokemon';
+import Search from "./components/Search";
 
 function App() {
     const [pokemons, setPokemons] = useState([]);
@@ -9,6 +10,7 @@ function App() {
     useEffect(() => {
         getAllPokemons()
           .then((response) => {
+            console.log(response.data.results)
             setPokemons(response.data.results)
           })
           .catch((err) => {
@@ -16,12 +18,13 @@ function App() {
           })
     }, [])
 
-    const list = pokemons.map((pokemon) => <Pokemon name={pokemon.name} />)
+    const list = pokemons.map((pokemon) => <Pokemon name={pokemon.name} key={pokemon.name} />)
 
     return (
         <div className="App">
             <header className="App-header">
-
+              <Search />
+              { list }
             </header>
         </div>
     );
---
+++ src/components/Search.js
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+const Search = () => {
+
+    const [value, setValue] = useState('')    
+
+    const [searchByType , setSearchByType] = useState(false)
+
+
+    return (
+        <div>
+            <input type='text' onChange={(e) => setValue(e.target.value) } />
+            <input type='checkbox' id="isByType" />
+            <label htmlFor="isByType" >Search by Type</label>
+            <button>Buscar</button>
+            
+        </div>
+    )
+}
+
+export default Search
